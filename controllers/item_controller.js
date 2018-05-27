9e2df1c1b68860561fb54d7a622e2de4b02fa7d0
unknown <tommyn.gre@gmail.com>
2018-05-27 17:34:57 -0400

    + frontend upgrades

--- controllers/item_controller.js
+++ controllers/item_controller.js
@@ -12,10 +12,31 @@ module.exports = function (app) {
   app.get("/", function (req, res) {
 
     item.all(function (data) {
+
+      data.forEach(row => {
+        let n = row.item_name;
+        let isURL = 'nope';
+        
+        //check if a url
+        if (n.substring(0, 3) == "htt" || n.substring(0, 3) == "www") {
+          row['URL'] = true;
+        } else {
+          row['URL'] = false;
+        }
+        //check length
+        if (n.length > 30) {
+          row['displayName'] = n.substring(0, 25)+'...';
+        } else {
+          row['displayName'] = n;
+        }
+      });
+
       let obj = {
         items: data
       };
+
       console.log(obj);
+
       res.render("index", obj);
     });
 
@@ -44,17 +65,16 @@ module.exports = function (app) {
         Boolean(req.body.isComplete)
       ],
       function (result) {
-        console.log("result "+result);
+        console.log("result " + result);
         res.json({ id: result.insertId });
-        console.log("result.insertId "+result.insertId);
-
+        console.log("result.insertId " + result.insertId);
       });
   });
 
   app.put("/api/list/:id", function (req, res) {
     let condition = "id = " + req.params.id;
 
-    console.log("req.body.isComplete "+req.body.isComplete);
+    console.log("req.body.isComplete " + req.body.isComplete);
 
     item.update({
       is_complete: req.body.isComplete
